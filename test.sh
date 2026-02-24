1add2f430714af194eb57619d5a3ef9973d194aa
jaiswamy <jaicswamy13@gmail.com>
2026-02-24 13:01:23 +0530

    final5

--- test.sh
+++ test.sh
@@ -12,5 +12,5 @@ sudo apt  install docker-compose
 # Allow ubuntu user to run docker
 sudo usermod -aG docker ubuntu
 
-# Install AWS CLI (for ECR pull)
-sudo apt install -y awscli
+sudo apt update
+sudo apt install awscli --classic -y
