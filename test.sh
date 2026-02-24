6289c6240d23a82235e097d5903455bb9a27fe88
jaiswamy <jaicswamy13@gmail.com>
2026-02-24 13:21:41 +0530

    final6

--- test.sh
+++ test.sh
@@ -9,8 +9,18 @@ sudo systemctl start docker
 
 sudo apt  install docker-compose
 
-# Allow ubuntu user to run docker
+
 sudo usermod -aG docker ubuntu
 
 sudo apt update
-sudo apt install awscli --classic -y
+sudo apt install unzip curl -y
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip
+sudo ./aws/install
+
+sudo curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
+-o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
